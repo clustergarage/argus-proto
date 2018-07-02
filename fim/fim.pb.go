4cb12f63bee1c4186f550ef3fb3cc5bbdf44de06
Brett Fowle <brettfowle@gmail.com>
2018-07-02 19:59:24 -0400

    s/NewWatcher/NewWatch

--- fim/fim.pb.go
+++ fim/fim.pb.go
@@ -35,7 +35,7 @@ func (m *FimdConfig) Reset()         { *m = FimdConfig{} }
 func (m *FimdConfig) String() string { return proto.CompactTextString(m) }
 func (*FimdConfig) ProtoMessage()    {}
 func (*FimdConfig) Descriptor() ([]byte, []int) {
-	return fileDescriptor_fim_c86b85db1aa93dd6, []int{0}
+	return fileDescriptor_fim_17469d5dc534ead0, []int{0}
 }
 func (m *FimdConfig) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_FimdConfig.Unmarshal(m, b)
@@ -81,7 +81,7 @@ func (m *FimWatcherSubject) Reset()         { *m = FimWatcherSubject{} }
 func (m *FimWatcherSubject) String() string { return proto.CompactTextString(m) }
 func (*FimWatcherSubject) ProtoMessage()    {}
 func (*FimWatcherSubject) Descriptor() ([]byte, []int) {
-	return fileDescriptor_fim_c86b85db1aa93dd6, []int{1}
+	return fileDescriptor_fim_17469d5dc534ead0, []int{1}
 }
 func (m *FimWatcherSubject) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_FimWatcherSubject.Unmarshal(m, b)
@@ -126,7 +126,7 @@ func (m *FimdHandler) Reset()         { *m = FimdHandler{} }
 func (m *FimdHandler) String() string { return proto.CompactTextString(m) }
 func (*FimdHandler) ProtoMessage()    {}
 func (*FimdHandler) Descriptor() ([]byte, []int) {
-	return fileDescriptor_fim_c86b85db1aa93dd6, []int{2}
+	return fileDescriptor_fim_17469d5dc534ead0, []int{2}
 }
 func (m *FimdHandler) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_FimdHandler.Unmarshal(m, b)
@@ -171,7 +171,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type FimdClient interface {
-	NewWatcher(ctx context.Context, in *FimdConfig, opts ...grpc.CallOption) (*FimdHandler, error)
+	NewWatch(ctx context.Context, in *FimdConfig, opts ...grpc.CallOption) (*FimdHandler, error)
 }
 
 type fimdClient struct {
@@ -182,9 +182,9 @@ func NewFimdClient(cc *grpc.ClientConn) FimdClient {
 	return &fimdClient{cc}
 }
 
-func (c *fimdClient) NewWatcher(ctx context.Context, in *FimdConfig, opts ...grpc.CallOption) (*FimdHandler, error) {
+func (c *fimdClient) NewWatch(ctx context.Context, in *FimdConfig, opts ...grpc.CallOption) (*FimdHandler, error) {
 	out := new(FimdHandler)
-	err := c.cc.Invoke(ctx, "/fim.Fimd/NewWatcher", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/fim.Fimd/NewWatch", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -193,27 +193,27 @@ func (c *fimdClient) NewWatcher(ctx context.Context, in *FimdConfig, opts ...grp
 
 // FimdServer is the server API for Fimd service.
 type FimdServer interface {
-	NewWatcher(context.Context, *FimdConfig) (*FimdHandler, error)
+	NewWatch(context.Context, *FimdConfig) (*FimdHandler, error)
 }
 
 func RegisterFimdServer(s *grpc.Server, srv FimdServer) {
 	s.RegisterService(&_Fimd_serviceDesc, srv)
 }
 
-func _Fimd_NewWatcher_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Fimd_NewWatch_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(FimdConfig)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FimdServer).NewWatcher(ctx, in)
+		return srv.(FimdServer).NewWatch(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/fim.Fimd/NewWatcher",
+		FullMethod: "/fim.Fimd/NewWatch",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FimdServer).NewWatcher(ctx, req.(*FimdConfig))
+		return srv.(FimdServer).NewWatch(ctx, req.(*FimdConfig))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -223,17 +223,17 @@ var _Fimd_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*FimdServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "NewWatcher",
-			Handler:    _Fimd_NewWatcher_Handler,
+			MethodName: "NewWatch",
+			Handler:    _Fimd_NewWatch_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "fim.proto",
 }
 
-func init() { proto.RegisterFile("fim.proto", fileDescriptor_fim_c86b85db1aa93dd6) }
+func init() { proto.RegisterFile("fim.proto", fileDescriptor_fim_17469d5dc534ead0) }
 
-var fileDescriptor_fim_c86b85db1aa93dd6 = []byte{
+var fileDescriptor_fim_17469d5dc534ead0 = []byte{
 	// 215 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x4c, 0xcb, 0xcc, 0xd5,
 	0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x4e, 0xcb, 0xcc, 0x55, 0x4a, 0xe2, 0xe2, 0x72, 0xcb,
@@ -245,8 +245,8 @@ var fileDescriptor_fim_c86b85db1aa93dd6 = []byte{
 	0x62, 0x2d, 0x48, 0x2c, 0xc9, 0x28, 0x96, 0x60, 0x54, 0x60, 0xd6, 0xe0, 0x0c, 0x82, 0x70, 0x84,
 	0xc4, 0xb8, 0xd8, 0x52, 0xcb, 0x52, 0xf3, 0xa0, 0x86, 0x73, 0x06, 0x41, 0x79, 0x4a, 0xb2, 0x5c,
 	0xdc, 0x20, 0x67, 0x7a, 0x24, 0xe6, 0xa5, 0xe4, 0xa4, 0x16, 0x09, 0xf1, 0x71, 0x31, 0x65, 0x42,
-	0x9c, 0xc7, 0x1a, 0xc4, 0x94, 0x99, 0x62, 0x64, 0xc9, 0xc5, 0x02, 0x92, 0x16, 0x32, 0xe4, 0xe2,
-	0xf2, 0x4b, 0x2d, 0x87, 0xda, 0x24, 0xc4, 0x0f, 0x73, 0x19, 0xd4, 0x7b, 0x52, 0x02, 0x70, 0x01,
-	0xa8, 0x41, 0x4a, 0x0c, 0x49, 0x6c, 0xe0, 0xc0, 0x30, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x66,
-	0x09, 0x05, 0x44, 0x19, 0x01, 0x00, 0x00,
+	0x9c, 0xc7, 0x1a, 0xc4, 0x94, 0x99, 0x62, 0x64, 0xce, 0xc5, 0x02, 0x92, 0x16, 0xd2, 0xe7, 0xe2,
+	0xf0, 0x4b, 0x2d, 0x07, 0xdb, 0x24, 0xc4, 0x0f, 0x73, 0x17, 0xd4, 0x73, 0x52, 0x02, 0x70, 0x01,
+	0xa8, 0x31, 0x4a, 0x0c, 0x49, 0x6c, 0xe0, 0xa0, 0x30, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0xf1,
+	0x6b, 0xe1, 0x66, 0x17, 0x01, 0x00, 0x00,
 }
